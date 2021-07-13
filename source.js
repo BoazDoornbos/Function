0e7bb6ad32284beb03a4f65f6920063c50fe6235
Boaz Doornbos <84346524+BoazDoornbos@users.noreply.github.com>
2021-07-13 21:53:55 +0200

    Initial commit

---
+++ source.js
@@ -0,0 +1,23 @@
+
+const makepizza = function () {
+    console.log('maak de bodem');
+    console.log('maak de bodem plat en rond');
+    console.log('pak de tomaten sous en leg dat op de bodem');
+    console.log('strooi er kaas over heen en leg er salami of groete er over heen');
+    console.log('leg alles in de oven op 220graden voor 10 min');
+
+};
+const makesuzi = function () {
+    console.log('pak de zee wier en de rijst');
+    console.log('leg de rijst in de zee wier ');
+    console.log('op de rijst leg je een stuk je vis');
+    console.log('nu kan je de suzie oprollen');
+    console.log('dit keer 3');
+
+};
+for (let i = 0; i < 3; i++) {
+    makesuzi();
+}
+for (let i = 0; i < 2; i++) {
+    makepizza();
+}
